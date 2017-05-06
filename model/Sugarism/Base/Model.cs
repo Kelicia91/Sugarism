5b26049db3ba472ff6575e65ea5c5f359920a0dc
Kelicia <kelicia91@gmail.com>
2017-05-06 19:25:04 +0900

    * add : model.base

---
+++ model/Sugarism/Base/Model.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.ComponentModel;
+
+
+namespace Sugarism.Base
+{
+    // @note
+    // UnityEngine supports .NET 3.5 basically,
+    // so can't use System.Runtime.CompilerServices.CallerMemberNameAttribute.
+    // (the namespace make me avoid to access property as string but supported by .NET 4.5+)
+    public class Model : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            VerifyPropertyName(propertyName);
+
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
+        }
+
+
+        protected virtual bool ThrowOnInvalidPropertyName { get; private set; }
+
+        [Conditional("DEBUG")]
+        [DebuggerStepThrough]
+        public void VerifyPropertyName(string propertyName)
+        {
+            // Verify that the property name matches a real,  
+            // public, instance property on this object.
+            if (TypeDescriptor.GetProperties(this)[propertyName] == null)
+            {
+                string msg = "Invalid property name: " + propertyName;
+
+                if (ThrowOnInvalidPropertyName)
+                    throw new Exception(msg);
+                else
+                    Debug.Fail(msg);
+            }
+        }
+    }
+}
