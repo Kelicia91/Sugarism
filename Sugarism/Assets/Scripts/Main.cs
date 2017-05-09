c3d8b9bda7cc19890e7ce3d461313e1f88ceb7ce
Kelicia <kelicia91@gmail.com>
2017-05-09 21:49:05 +0900

    * add : Main script

---
+++ Sugarism/Assets/Scripts/Main.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Main : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+    {
+        bool isLoaded = false;
+
+        Dictionary<int, Sugarism.Character> characterDict = null;
+        isLoaded = RsrcLoader.Instance.Load(Configuration.CHARACTER_FILE_PATH, out characterDict);
+        if (true == isLoaded)
+        {
+            Log.Debug("success to load character dict");
+            foreach(KeyValuePair<int, Sugarism.Character> kv in characterDict)
+            {
+                string s = string.Format("{0}:{1}", kv.Key, kv.Value.Name);
+                Log.Debug(s);
+            }
+        }
+        else
+        {
+            Log.Error("failed to load character dict");
+        }
+
+
+        List<Scenario> scenarioList = null;
+        isLoaded = RsrcLoader.Instance.Load(Configuration.SCENARIO_FOLDER_PATH, out scenarioList);
+        if (true == isLoaded)
+        {
+            Log.Debug("success to load scenario list");
+            foreach (Scenario s in scenarioList)
+            {
+                s.Execute();
+            }
+        }
+        else
+        {
+            Log.Error("failed to load scenario list");
+        }
+	}
+}
