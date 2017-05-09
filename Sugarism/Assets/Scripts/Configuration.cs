9f9204a5eb2ba105100e383cdda5ef823cab0426
Kelicia <kelicia91@gmail.com>
2017-05-09 21:20:49 +0900

    * add : configuration (file path)

---
+++ Sugarism/Assets/Scripts/Configuration.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Configuration
+{
+    // @note : UnityEngine.Resources
+    // - The path is relative to any Resources folder inside the Assets folder of your project, 
+    //  extensions must be omitted.
+    // - All asset names and paths in Unity use forward slashes, 
+    //  paths using backslashes will not work.
+
+    public const string CHARACTER_FILE_PATH = "sample";
+    public const string SCENARIO_FOLDER_PATH = "Scenarios";
+
+    //
+}
