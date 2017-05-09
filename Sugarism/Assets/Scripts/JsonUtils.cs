886f7da49b2c4169c3bd55437597409f3570ece2
Kelicia <kelicia91@gmail.com>
2017-05-09 19:15:05 +0900

    * add : JsonUtils, lib for Newtonsoft.Json

---
+++ Sugarism/Assets/Scripts/JsonUtils.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Newtonsoft.Json;
+
+
+public class JsonUtils
+{
+    public static bool Serialize(object o, out string s, JsonSerializerSettings settings)
+    {
+        s = null;
+
+        try
+        {
+            s = JsonConvert.SerializeObject(o, settings);
+        }
+        catch(Exception e)
+        {
+            Log.Error(e.Message);
+            return false;
+        }
+
+        return true;
+    }
+
+    public static bool Deserialize<T>(string s, out object o, JsonSerializerSettings settings)
+    {
+        o = null;
+
+        try
+        {
+            o = JsonConvert.DeserializeObject<T>(s, settings);
+        }
+        catch(Exception e)
+        {
+            Log.Error(e.Message);
+            return false;
+        }
+
+        return true;
+    }
+}
