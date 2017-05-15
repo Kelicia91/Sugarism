e1b6846bacefbe37e8254964e7b4358beea88260
Kelicia <kelicia91@gmail.com>
2017-05-16 01:15:33 +0900

    * add : separate prefab 'ClearPanel' from Canvas * mod : add OnPointerClick event

--- Sugarism/Assets/Scripts/UI/ClearPanel.cs
+++ Sugarism/Assets/Scripts/UI/ClearPanel.cs
@@ -12,8 +12,11 @@ public class ClearPanel : MonoBehaviour, IPointerClickHandler
 		
 	}
 
+    // 오브젝트에서 포인터를 누르고 동일한 오브젝트에서 뗄 때 호출됩니다.
     void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
     {
         Log.Debug("clicked clear panel");
+
+        //ObjectManager.Instance.NextCmd();
     }
 }
