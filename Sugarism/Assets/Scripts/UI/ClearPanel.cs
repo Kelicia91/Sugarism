6ba49049db952c7f3e0181d0cd00e3c6a0f471e4
Kelicia <kelicia91@gmail.com>
2017-05-15 13:22:48 +0900

    * add : UI script folder, ClearPanel script(implements IPointerClickHandler)

---
+++ Sugarism/Assets/Scripts/UI/ClearPanel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class ClearPanel : MonoBehaviour, IPointerClickHandler
+{
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+    {
+        Log.Debug("clicked clear panel");
+    }
+}
