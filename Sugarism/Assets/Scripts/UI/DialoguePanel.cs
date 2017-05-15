4d26e0e283b4ea78926c483930b3a323c4632645
Kelicia <kelicia91@gmail.com>
2017-05-16 01:26:18 +0900

    * add : dialogue panel prefab/script, main canvas script * mod : attach main canvas script to canvas prefab

---
+++ Sugarism/Assets/Scripts/UI/DialoguePanel.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DialoguePanel : MonoBehaviour
+{
+    public Text NameText;
+    public Text LinesText;
+
+
+	// Use this for initialization
+	void Start()
+    {
+        CustomEvent.Instance.Attach(OnCmdLines);
+	}
+
+    //
+    void OnDestroy()
+    {
+        CustomEvent.Instance.Detach(OnCmdLines);
+    }
+
+    
+
+    private void setName(string s)
+    {
+        if (null == NameText)
+            Log.Error("not found name text");
+        else
+            NameText.text = s;
+    }
+
+    private void setLines(string s)
+    {
+        if (null == LinesText)
+            Log.Error("not found lines text");
+        else
+            LinesText.text = s;
+    }
+
+
+    // CmdLinesEventHandler
+    void OnCmdLines(int characterId, string lines)
+    {
+        Sugarism.Character ch = ObjectManager.Instance.Get(characterId);
+        if (null == ch)
+        {
+            string errMsg = string.Format("not found character {0}", characterId);
+            Log.Error(errMsg);
+            return;
+        }
+
+        setName(ch.Name);
+        setLines(lines);
+    }
+}
---
+++ Sugarism/Assets/Scripts/UI/MainCanvas.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MainCanvas : MonoBehaviour
+{
+    // prefabs
+    public GameObject DialoguePanel;
+    public GameObject ClearPanel;
+    
+    // Use this for initialization
+    void Start ()
+    {        
+        // @warn : call order of instantiate()
+        Instantiate(DialoguePanel, gameObject.transform);
+        Instantiate(ClearPanel, gameObject.transform);
+	}
+}
