e6585cba5a89d16b547234f81861e7ee6b2809b2
Kelicia <kelicia91@gmail.com>
2017-06-05 17:23:24 +0900

    * mod : animation bug comment

--- Sugarism/Assets/Scripts/UI/RunScheduleAnimController.cs
+++ Sugarism/Assets/Scripts/UI/RunScheduleAnimController.cs
@@ -28,13 +28,19 @@ public class RunScheduleAnimController : MonoBehaviour
     //
     private Animator _animator;
 
-
     //
     void Awake()
     {
         _animator = GetComponent<Animator>();
     }
-    
+
+    //
+    void OnEnable()
+    {
+        //SetTrigger(ETrigger.Finish);  // 제대로 안먹힌다..
+        Log.Debug("RunScheduleAnimController.OnEnable");
+    }
+
     public void SetTrigger(ETrigger triggerType)
     {
         if (ETrigger.MAX == triggerType)
@@ -47,18 +53,21 @@ public class RunScheduleAnimController : MonoBehaviour
     }
 
     private string _prevTrigger = string.Empty;
-
+    //private const string ANIM_PARAM_NAME = "stateNo";
     private void setTrigger(ETrigger triggerType)
     {
-        // @note : id is HASH......
-        //int id = (int)triggerType;
-        //_animator.SetTrigger(id);
-
         _animator.ResetTrigger(_prevTrigger);
 
         string s = triggerType.ToString();
         _animator.SetTrigger(s);
 
         _prevTrigger = s;
+
+        // int 변수 하나로 제어해도 disable 이 finish 보다 먼저 일어나는지..
+        // 마지막 행동에 대해서만 finish 가 제대로 안 먹혀서
+        // 다음에 다시 애니메이션 시작하려고 하면 마지막 행동 상태에서 여전히 남아있음..
+        // 해결해야 하는 문제.. 고로 anim state 관리를 trigger 하든 int 하든 해결방법이 못됨.
+        //int value = (int)triggerType;
+        //_animator.SetInteger(ANIM_PARAM_NAME, value);
     }
 }
