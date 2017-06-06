df385d72fe37073d18fb628bef34af9c1a733aa3
Kelicia <kelicia91@gmail.com>
2017-06-06 19:28:14 +0900

    * add : extended npc scripts

---
+++ Sugarism/Assets/Scripts/model/ExtNPC.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ExtNPC
+{
+    public static bool IsValid(int id)
+    {
+        if (id < 0)
+            return false;
+        else if (id >= Manager.Instance.DTNPC.Count)
+            return false;
+        else
+            return true;
+    }
+}
