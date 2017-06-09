5496c38d9ecb1be50d85b6c79e8cc8fc26bfafa3
Kelicia <kelicia91@gmail.com>
2017-06-09 17:29:14 +0900

    * add : model for referencing game datatable(.xml)

---
+++ tools/ScenarioEditor/ScenarioEditor/Model/ArtsResource.cs
@@ -0,0 +1,38 @@
+﻿
+namespace ScenarioEditor.Model
+{
+    // for referencing Art(image, sound, ..) DataTable(.xml)
+    public class ArtsResource : ModelBase
+    {
+        public const string XML_COLUMN_NAME = "description";
+
+        public const string XML_BACKGROUND_NAME = "Background";
+        public const string XML_MINIPICTURE_NAME = "MiniPicture";
+        public const string XML_PICTURE_NAME = "Picture";
+        public const string XML_SE_NAME = "SE";
+
+
+        private int _id;
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; OnPropertyChanged(); }
+        }
+
+        private string _description;
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; OnPropertyChanged(); }
+        }
+
+
+        public ArtsResource() : this(-1, string.Empty) { }
+
+        public ArtsResource(int id, string description)
+        {
+            _id = id;
+            _description = description;
+        }
+    }
+}
---
+++ tools/ScenarioEditor/ScenarioEditor/Model/Character.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ScenarioEditor.Model
+{
+    // @note: maybe don't need to use Sugarism.Character...
+
+    // for referencing Character DataTable(.xml)
+    public class Character : ModelBase
+    {
+        // const
+        public const int START_ID = 0;
+        public const string STR_UNKNOWN = "unknown";
+
+        public const string XML_COLUMN_NAME = "name";
+        public const string XML_CHARACTER_NAME = "Character";
+
+
+        private int _id;
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; OnPropertyChanged(); }
+        }
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; OnPropertyChanged(); }
+        }
+
+
+        public Character() : this(-1, string.Empty) { }
+
+        public Character(int id, string name)
+        {
+            _id = id;
+            _name = name;
+        }
+    }
+}
