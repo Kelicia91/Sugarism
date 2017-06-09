9cd5e5c8abb4803267898b69e41a291ce860bd33
Kelicia <kelicia91@gmail.com>
2017-06-09 17:13:44 +0900

    * add : cmd fileter viewmodel

---
+++ Sugarism/Assets/Scripts/sugarism/CmdFilter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CmdFilter : Command
+{
+    private Sugarism.CmdFilter _model;
+
+	public CmdFilter(Sugarism.CmdFilter model) : base(model)
+    {
+        _model = model;
+    }
+
+
+    #region Property
+
+    public Sugarism.EFilter Filter
+    {
+        get { return _model.Filter; }
+    }
+
+    #endregion
+
+
+    public override void Execute()
+    {
+        Log.Debug(ToString());
+    }
+
+    public override bool Play()
+    {
+        Log.Debug(ToString());
+
+        Manager.Instance.CmdFilterEvent.Invoke(Filter);
+
+        return false;   // no more child to play
+    }
+}
