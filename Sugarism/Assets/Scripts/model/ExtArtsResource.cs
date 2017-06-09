f9281892c422113ddc25fa993849d7cb280f3d24
Kelicia <kelicia91@gmail.com>
2017-06-09 22:57:07 +0900

    * add : for validating model id

---
+++ Sugarism/Assets/Scripts/model/ExtArtsResource.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtBackground
+{
+    public static bool IsValid(int id)
+    {
+        if (id < 0)
+            return false;
+        else if (id >= Manager.Instance.DTBackground.Count)
+            return false;
+        else
+            return true;
+    }
+}
+
+public class ExtMiniPicture
+{
+    public static bool IsValid(int id)
+    {
+        if (id < 0)
+            return false;
+        else if (id >= Manager.Instance.DTMiniPicture.Count)
+            return false;
+        else
+            return true;
+    }
+}
+
+public class ExtPicture
+{
+    public static bool IsValid(int id)
+    {
+        if (id < 0)
+            return false;
+        else if (id >= Manager.Instance.DTPicture.Count)
+            return false;
+        else
+            return true;
+    }
+}
+
+public class ExtSE
+{
+    public static bool IsValid(int id)
+    {
+        if (id < 0)
+            return false;
+        else if (id >= Manager.Instance.DTSE.Count)
+            return false;
+        else
+            return true;
+    }
+}
