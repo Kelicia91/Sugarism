e5d8625c2119f258527a6aec05dca54fc80064e8
Kelicia <kelicia91@gmail.com>
2017-06-09 22:59:16 +0900

    * add : mini picture panel.cs

---
+++ Sugarism/Assets/Scripts/UI/MiniPicturePanel.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MiniPicturePanel : Panel
+{
+    /********* Editor Interface *********/
+    // prefabs
+    public Image Image;
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        Manager.Instance.CmdMiniPictureEvent.Attach(onCmdMiniPicture);
+
+        Hide();
+	}
+
+
+    private void set(Sprite s)
+    {
+        if (null == Image)
+        {
+            Log.Error("not found mini picture image");
+            return;
+        }
+
+        Image.sprite = s;
+    }
+
+    private void onCmdMiniPicture(int id)
+    {
+        if (false == ExtMiniPicture.IsValid(id))
+            return;
+
+        MiniPicture p = Manager.Instance.DTMiniPicture[id];
+        set(p.sprite);
+
+        Show();
+    }
+}
