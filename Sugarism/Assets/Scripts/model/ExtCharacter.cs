b364ed4b6b4d38be6c93c402295af4ae57b2aad8
Kelicia <kelicia91@gmail.com>
2017-06-09 17:12:41 +0900

    * add : script extended character for validating character id

---
+++ Sugarism/Assets/Scripts/model/ExtCharacter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtCharacter
+{
+    public static bool IsValid(int id)
+    {
+        if (id < 0)
+            return false;
+        else if (id >= Manager.Instance.DTCharacter.Count)
+            return false;
+        else
+            return true;
+    }
+}
