8a3457bd11d11a4236bc4a1297d4c969f4430f54
Kelicia <kelicia91@gmail.com>
2017-06-16 10:55:47 +0900

    * add : validation scripts for board game player datatable

---
+++ Sugarism/Assets/Scripts/model/ExtBoardGamePlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtBoardGamePlayer
+{
+    public static bool isValid(int id)
+    {
+        if (id < 0)
+            return false;
+        else if (id >= Manager.Instance.DTBoardGamePlayer.Count)
+            return false;
+        else
+            return true;
+    }
+}
