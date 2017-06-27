c62313787231f98f5e0d01946e6d3888f55ef929
Kelicia <kelicia91@gmail.com>
2017-06-27 19:27:38 +0900

    * rename : ExtNPC -> ExtActionNPC .cs

--- Sugarism/Assets/Scripts/model/ExtActionNPC.cs
+++ Sugarism/Assets/Scripts/model/ExtActionNPC.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class ExtNPC
+public class ExtActionNPC
 {
     public static bool IsValid(int id)
     {
         if (id < 0)
             return false;
-        else if (id >= Manager.Instance.DTNPC.Count)
+        else if (id >= Manager.Instance.DTActionNPC.Count)
             return false;
         else
             return true;
