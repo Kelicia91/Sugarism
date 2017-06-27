ebddb8ac789f7fe6616674d1ab97c2a31822cf86
Kelicia <kelicia91@gmail.com>
2017-06-27 19:01:25 +0900

    * add : Exam abstract class

---
+++ Sugarism/Assets/Scripts/Nurture/Exam.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Exam
+{
+    public enum EType
+    {
+        COMBAT = 0,
+        BOARD_GAME,
+        SCORE,
+
+        MAX
+    }
+
+    // Nurture.Action.Lesson <---> (Exam) <---> Mode
+    public abstract class Exam
+    {
+        //
+        private EType _type = EType.MAX;
+        public EType Type { get { return _type; } }
+
+        private bool _isFirst = false;
+        public bool IsFirst { get { return _isFirst; } }
+
+        private int _npcId = -1;
+        public int NPCId { get { return _npcId; } }
+
+        //
+        private IEnumerator _iterator = null;
+
+
+        #region Events
+
+        private StartEvent _startEvent = null;
+        public StartEvent StartEvent { get { return _startEvent; } }
+
+        private EndEvent _endEvent = null;
+        public EndEvent EndEvent { get { return _endEvent; } }
+
+        private DialogueEvent _dialogueEvent = null;
+        public DialogueEvent DialogueEvent { get { return _dialogueEvent; } }
+
+        #endregion
+
+
+        // constructor
+        protected Exam(EType type, bool isFirst, int npcId)
+        {
+            _type = type;
+            _isFirst = isFirst;
+            _npcId = npcId;
+
+            _iterator = null;
+
+            _startEvent = new StartEvent();
+            _endEvent = new EndEvent();
+            _dialogueEvent = new DialogueEvent();
+        }
+
+        public void Start()
+        {
+            start();
+            Iterate();
+        }
+
+        private void start()
+        {
+            _iterator = ExamRoutine();
+            StartEvent.Invoke();
+        }
+
+        private void end()
+        {
+            _iterator = null;
+            EndEvent.Invoke();
+        }
+
+        public void Iterate()
+        {
+            if (null == _iterator)
+            {
+                Log.Error("not found iterator");
+                return;
+            }
+
+            if (_iterator.MoveNext())
+                return;
+
+            end();
+        }
+
+        protected abstract IEnumerator ExamRoutine();
+
+    }   // class
+
+}   // namespace
---
+++ Sugarism/Assets/Scripts/Nurture/ExamEvent.cs
@@ -0,0 +1,137 @@
+﻿
+namespace Exam
+{
+    public class StartEvent
+    {
+        public delegate void Handler();
+        private event Handler _event = null;
+
+        public StartEvent()
+        {
+            _event = new Handler(onStart);
+        }
+
+        // default handler
+        private void onStart()
+        {
+            Log.Debug(string.Format("Exam.onStart;"));
+        }
+
+        public void Invoke() { _event.Invoke(); }
+
+        public void Attach(Handler handler)
+        {
+            if (null == handler)
+                return;
+
+            _event += handler;
+        }
+
+        public void Detach(Handler handler)
+        {
+            if (null == handler)
+                return;
+
+            _event -= handler;
+        }
+    }
+
+    public class EndEvent
+    {
+        public delegate void Handler();
+        private event Handler _event = null;
+
+        public EndEvent()
+        {
+            _event = new Handler(onEnd);
+        }
+
+        // default handler
+        private void onEnd()
+        {
+            Log.Debug(string.Format("Exam.onEnd;"));
+        }
+
+        public void Invoke() { _event.Invoke(); }
+
+        public void Attach(Handler handler)
+        {
+            if (null == handler)
+                return;
+
+            _event += handler;
+        }
+
+        public void Detach(Handler handler)
+        {
+            if (null == handler)
+                return;
+
+            _event -= handler;
+        }
+    }
+
+    public class DialogueEvent
+    {
+        public delegate void Handler(string lines);
+        private event Handler _event = null;
+
+        public delegate void NPCHandler(int npcId, string lines);
+        private event NPCHandler _npcEvent = null;
+
+        public DialogueEvent()
+        {
+            _event = new Handler(onDialogue);
+            _npcEvent = new NPCHandler(onDialogueNPC);
+        }
+
+        // default handler
+        private void onDialogue(string lines)
+        {
+            Log.Debug(string.Format("Exam.onDialogue; {0}", lines));
+        }
+
+        public void Invoke(string lines) { _event.Invoke(lines); }
+
+        public void Attach(Handler handler)
+        {
+            if (null == handler)
+                return;
+
+            _event += handler;
+        }
+
+        public void Detach(Handler handler)
+        {
+            if (null == handler)
+                return;
+
+            _event -= handler;
+        }
+
+        // default handler
+        private void onDialogueNPC(int npcId, string lines)
+        {
+            Log.Debug(string.Format("Exam.onDialogue; npcId({0}): {1}", npcId, lines));
+        }
+
+        public void Invoke(int npcId, string lines) { _npcEvent.Invoke(npcId, lines); }
+
+        public void Attach(NPCHandler handler)
+        {
+            if (null == handler)
+                return;
+
+            _npcEvent += handler;
+        }
+
+        public void Detach(NPCHandler handler)
+        {
+            if (null == handler)
+                return;
+
+            _npcEvent -= handler;
+        }
+    }
+
+}   // namespace
