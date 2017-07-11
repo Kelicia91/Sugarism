40085672fb4d1ba3ea133f1401070481f6442388
Kelicia <kelicia91@gmail.com>
2017-07-12 00:24:30 +0900

    * add : cmd button script

---
+++ Sugarism/Assets/Scripts/UI/CmdButton.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CmdButton : MonoBehaviour
+{
+    // prefabs
+    public Image IconImage;
+    public Text Text;
+
+    //
+    private Button _button = null;
+
+    //
+    void Awake()
+    {
+        _button = GetComponent<Button>();
+    }
+
+    public void AddClickListener(UnityEngine.Events.UnityAction clickHandler)
+    {
+        if (null == _button)
+        {
+            Log.Error("not found button component");
+            return;
+        }
+
+        if (null == clickHandler)
+        {
+            Log.Error("not found click handler");
+            return;
+        }
+
+        _button.onClick.AddListener(clickHandler);
+    }
+
+    public void SetIcon(Sprite s)
+    {
+        if (null == IconImage)
+        {
+            Log.Error("not found cmd button's icon image");
+            return;
+        }
+
+        if (null == s)
+        {
+            Log.Error("not found icon sprite");
+            return;
+        }
+
+        IconImage.sprite = s;
+    }
+
+    public void SetText(string s)
+    {
+        if (null == Text)
+        {
+            Log.Error("not found cmd button's text");
+            return;
+        }
+
+        if (null == s)
+        {
+            Log.Error("not found string for text");
+            return;
+        }
+
+        Text.text = s;
+    }
+}
