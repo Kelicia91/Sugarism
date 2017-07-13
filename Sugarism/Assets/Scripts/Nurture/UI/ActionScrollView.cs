6611a61b3075ad619d5f5079c9d240d49934ace1
Kelicia <kelicia91@gmail.com>
2017-07-13 12:28:58 +0900

    * mod : re-layout action buttons

--- Sugarism/Assets/Scripts/Nurture/UI/ActionScrollView.cs
+++ Sugarism/Assets/Scripts/Nurture/UI/ActionScrollView.cs
@@ -11,15 +11,11 @@ public class ActionScrollView : Panel
     public GameObject PrefActionButton;
 
     //
-    public float PADDING_X;
-    public float PADDING_Y;
-    public float SPACE_X_BUTTON;
-    public float SPACE_Y_BUTTON;
-
-    //
     private EActionType _actionType = EActionType.MAX;
-    private RectTransform _viewport = null;
+
     private RectTransform _content = null;
+    private GridLayoutGroup _gridLayoutGroup = null;
+
     private GameObject[] _childObject = null;
 
 
@@ -29,8 +25,9 @@ public class ActionScrollView : Panel
         ScrollRect scrollRect = GetComponent<ScrollRect>();
         if (null != scrollRect)
         {
-            _viewport = scrollRect.GetComponent<RectTransform>();
             _content = scrollRect.content;
+
+            _gridLayoutGroup = _content.gameObject.GetComponent<GridLayoutGroup>();
         }
         else
         {
@@ -42,16 +39,11 @@ public class ActionScrollView : Panel
     void Start()
     {
         if (null == PrefActionButton)
-            Log.Error("not found prefab action button"); 
+            Log.Error("not found prefab action button");
         else
-            createActionButton();
+            createActionButtons();
 
-        if (null == _viewport)
-            Log.Error("not found viewport");
-        else if (null == _content)
-            Log.Error("not found content");
-        else
-            layout();
+        setContentHeight();
     }
 
     // @note : called before Start(), after OnEnabled()
@@ -66,7 +58,7 @@ public class ActionScrollView : Panel
         _actionType = actionType;
     }
 
-    private void createActionButton()
+    private void createActionButtons()
     {
         List<int> actionIdList = null;
         get(_actionType, out actionIdList);
@@ -89,89 +81,50 @@ public class ActionScrollView : Panel
             btn.SetActionId(actionIdList[i]);
         }
     }
-
-    // @note : Call after Drawing, because Get Stretched RectTransfrom Size.
-    private void layout()
+    
+    // Calculate content's height for activating vertical scrollbar
+    private void setContentHeight()
     {
-        int numChildren = _childObject.Length;
-        if (numChildren <= 0)
+        if (null == _gridLayoutGroup)
         {
-            Log.Error("invalid num of child object");
-            return;
-        }
-        
-        // viewport (w,h)
-        float viewWidth = _viewport.rect.width;
-        float viewHeight = _viewport.rect.height;
-
-        string msg = string.Format("viewport (w,h) = ({0},{1})", viewWidth, viewHeight);
-        Log.Debug(msg);
-
-        // button (w,h)
-        RectTransform btnRectTransform = PrefActionButton.GetComponent<RectTransform>();
-        float btnWidth = btnRectTransform.rect.width;
-        float btnHeight = btnRectTransform.rect.height;
-                
-        // child object area (w,h)
-        float deltaWidth = btnWidth + SPACE_X_BUTTON;
-        if (deltaWidth <= 0)
-        {
-            Log.Debug("failed to layout; btnWidth + SPACE_X_BUTTON <= 0");
+            Log.Error("not found grid layout group");
             return;
         }
 
-        float deltaHeight = btnHeight + SPACE_Y_BUTTON;
-        if (deltaHeight <= 0)
+        if (null == _content)
         {
-            Log.Debug("failed to layout; btnHeight + SPACE_Y_BUTTON <= 0");
+            Log.Error("not found content");
             return;
         }
 
-        // num(Column) in viewport
-        float quotient = (viewWidth - (2 * PADDING_X) + SPACE_X_BUTTON) / deltaHeight;
-        int numViewColumn = Mathf.FloorToInt(quotient);
-        
-        // num(Column) in content
-        int numColumn = numViewColumn;
-        if (numColumn <= 0)
+        RectTransform contentRect = _content.gameObject.GetComponent<RectTransform>();
+
+        int buttonCount = _childObject.Length;
+
+        float viewportWidth = GetComponent<RectTransform>().rect.width;
+        viewportWidth -= _gridLayoutGroup.padding.left;
+        viewportWidth -= _gridLayoutGroup.padding.right;
+        viewportWidth += _gridLayoutGroup.spacing.x;
+        int columnCountInGrid = (int) (viewportWidth / (_gridLayoutGroup.cellSize.x + _gridLayoutGroup.spacing.x));
+
+        if (columnCountInGrid <= 0)
         {
-            Log.Debug("too small viewport.width; num(Column) <= 0");
-            numColumn = 1;
+            Log.Error("invalid column(acton button) count");
+            return;
         }
 
-        // num(Row) in content
-        int numRow = numChildren / numColumn;
-        int remainder = numChildren % numColumn;
+        int rowCount = buttonCount / columnCountInGrid;
+
+        int remainder = buttonCount % columnCountInGrid;
         if (remainder > 0)
-            numRow += 1;
+            ++rowCount;
 
-        // content size; anchor (stretch,center)
-        float contentHeight = (2 * PADDING_Y) + (deltaHeight * numRow) - SPACE_Y_BUTTON;
-        _content.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, contentHeight);
+        float width = contentRect.sizeDelta.x;
+        float height = _gridLayoutGroup.padding.top + _gridLayoutGroup.padding.bottom
+                    + (_gridLayoutGroup.cellSize.y * rowCount)
+                    + (_gridLayoutGroup.spacing.y * (rowCount - 1));
 
-        // child; anchor (top,left), pivot (0,1)
-        float posX = 0.0f;
-        float posY = 0.0f;
-        for (int i = 0; i < numChildren; ++i)
-        {
-            if (0 == i)
-            {
-                posX = PADDING_X;
-                posY = -1 * PADDING_Y;
-            }
-            else if (i % numColumn == 0)
-            {
-                posX = PADDING_X;
-                posY -= deltaHeight;
-            }
-            else
-            {
-                posX += deltaWidth;
-            }
-
-            RectTransform rt = _childObject[i].GetComponent<RectTransform>();
-            rt.anchoredPosition = new Vector2(posX, posY);
-        }
+        contentRect.sizeDelta = new Vector2(width, height);
     }
 
     private void get(EActionType actionType, out List<int> actionIdList)
