880bfee194b011bddb7591676008f3f879c808c4
Kelicia <kelicia91@gmail.com>
2017-07-13 12:27:19 +0900

    * mod : remove to create currency panel

--- Sugarism/Assets/Scripts/Nurture/UI/SchedulePanel.cs
+++ Sugarism/Assets/Scripts/Nurture/UI/SchedulePanel.cs
@@ -9,7 +9,6 @@ public class SchedulePanel : Panel
     /********* Editor Interface *********/
     // prefabs
     public GameObject PrefBackButton;
-    public GameObject PrefCurrencyPanel;
     public GameObject PrefStatPanel;
     public GameObject StatsPanel;
     public ActionListPanel ActionListPanel;
@@ -42,16 +41,6 @@ public class SchedulePanel : Panel
         else
         {
             Log.Error("not found prefab back button");
-        }
-
-        if (null != PrefCurrencyPanel)
-        {
-            GameObject o = Instantiate(PrefCurrencyPanel);
-            o.transform.SetParent(transform, false);
-        }
-        else
-        {
-            Log.Error("not found prefab currency panel");
         }
 
         if (null != PrefStatPanel)
