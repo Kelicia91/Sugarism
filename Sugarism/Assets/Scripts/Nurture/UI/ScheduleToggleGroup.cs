0805bcbee23a333d7cfb440e2844303c5f8256cb
Kelicia <kelicia91@gmail.com>
2017-07-13 12:21:38 +0900

    * mod : remove layout hard coding

--- Sugarism/Assets/Scripts/Nurture/UI/ScheduleToggleGroup.cs
+++ Sugarism/Assets/Scripts/Nurture/UI/ScheduleToggleGroup.cs
@@ -11,10 +11,7 @@ public class ScheduleToggleGroup : MonoBehaviour
     public GameObject PrefScheduleToggle;
 
     //
-    public int SPACE_X_BETWEEN_TOGGLE = 10;
-
-    //
-    private ToggleGroup _toggleGroup;
+    private ToggleGroup _toggleGroup = null;
 
     //
     void Awake()
@@ -23,7 +20,7 @@ public class ScheduleToggleGroup : MonoBehaviour
     }
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
         if (null == PrefScheduleToggle)
         {
@@ -31,21 +28,10 @@ public class ScheduleToggleGroup : MonoBehaviour
             return;
         }
 
-        RectTransform rectTransform = PrefScheduleToggle.GetComponent<RectTransform>();
-        float childWidth = rectTransform.rect.width;
-
-        int halfNumOfAction = Def.MAX_NUM_ACTION_IN_MONTH / 2;
-        float space_posX = childWidth + SPACE_X_BETWEEN_TOGGLE;
-
-        float posX = -1 * space_posX * halfNumOfAction;
 	    for (int i = 0; i < Def.MAX_NUM_ACTION_IN_MONTH; ++i)
         {
             GameObject o = Instantiate(PrefScheduleToggle);
             o.transform.SetParent(transform, false);
-
-            RectTransform rect = o.GetComponent<RectTransform>();
-            rect.anchoredPosition = new Vector2(posX, 0.0f);
-            posX += space_posX;
 
             ScheduleToggle toggle = o.GetComponent<ScheduleToggle>();
             toggle.ScheduleIndex = i;
