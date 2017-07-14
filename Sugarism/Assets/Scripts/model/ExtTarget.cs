ddd9f9b783f0a698898033197d845d90a5cd7d15
Kelicia <kelicia91@gmail.com>
2017-07-14 15:46:07 +0900

    * add : ext target - validate target table id

---
+++ Sugarism/Assets/Scripts/model/ExtTarget.cs
@@ -0,0 +1,13 @@
+﻿
+public class ExtTarget
+{
+    public static bool isValid(int id)
+    {
+        if (id < 0)
+            return false;
+        else if (id >= Manager.Instance.DTTarget.Count)
+            return false;
+        else
+            return true;
+    }
+}
