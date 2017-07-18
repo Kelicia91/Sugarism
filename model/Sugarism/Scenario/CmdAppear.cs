f3862e503bac18f1df38157c1cc0281795bfdb90
Kelicia <kelicia91@gmail.com>
2017-07-19 00:44:37 +0900

    * mod : cmd appear - remove face, costume property

--- model/Sugarism/Scenario/CmdAppear.cs
+++ model/Sugarism/Scenario/CmdAppear.cs
@@ -1,25 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 namespace Sugarism
 {
-    public enum EFace
-    {
-        Normal,
-        Angry,
-        Happy,
-        Surprise,
-        Shy
-    }
-
-    public enum ECostume
-    {
-        Normal,
-        Special // temp value
-    }
-
     public enum EPosition
     {
         Middle = 0,
@@ -33,28 +14,14 @@ namespace Sugarism
     public class CmdAppear : Command
     {
         // property
-        private int _characterId;
+        private int _characterId = -1;
         public int CharacterId
         {
             get { return _characterId; }
             set { _characterId = value; OnPropertyChanged("CharacterId"); }
         }
 
-        private EFace _face;
-        public EFace Face
-        {
-            get { return _face; }
-            set { _face = value; OnPropertyChanged("Face"); }
-        }
-
-        private ECostume _costume;
-        public ECostume Costume
-        {
-            get { return _costume; }
-            set { _costume = value; OnPropertyChanged("Costume"); }
-        }
-
-        private EPosition _position;
+        private EPosition _position = EPosition.Middle;
         public EPosition Position
         {
             get { return _position; }
@@ -63,13 +30,13 @@ namespace Sugarism
 
 
         // default constructor for JSON Deserializer
-        public CmdAppear() : this(-1, EFace.Normal, ECostume.Normal, EPosition.Middle) { }
+        public CmdAppear() : this(-1) { }
 
-        public CmdAppear(int characterId, EFace face, ECostume costume, EPosition pos) : base(Command.Type.Appear)
+        public CmdAppear(int characterId) : this(characterId, EPosition.Middle) { }
+
+        public CmdAppear(int characterId, EPosition pos) : base(Command.Type.Appear)
         {
             _characterId = characterId;
-            _face = face;
-            _costume = costume;
             _position = pos;
         }
     }
