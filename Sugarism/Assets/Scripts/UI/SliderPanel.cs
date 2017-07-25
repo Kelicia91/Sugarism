f980e6cf8ceef92a16da3623acfef90df13f05ef
Kelicia <kelicia91@gmail.com>
2017-07-25 22:35:39 +0900

    * add : slider panel script

---
+++ Sugarism/Assets/Scripts/UI/SliderPanel.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SliderPanel : MonoBehaviour
+{
+    /********* Editor Interface *********/
+    // prefabs
+    public Text NameText;
+    public Slider Slider;
+    public Text ValueText;
+
+
+    //
+    void Awake()
+    {
+        if (null != Slider)
+        {
+            Slider.interactable = false;
+            Slider.wholeNumbers = true;
+            Slider.minValue = Def.MIN_STAT;
+            Slider.maxValue = Def.MAX_STAT;
+            Slider.value = Slider.minValue;   
+        }
+    }
+    
+    public void SetNameText(string s)
+    {
+        if (null == NameText)
+        {
+            Log.Error("not found name text");
+            return;
+        }
+
+        NameText.text = s;
+    }
+
+    public void SetValue(int value, string format = "{0}")
+    {
+        if (null == Slider)
+        {
+            Log.Error("not found slider");
+            return;
+        }
+
+        Slider.value = value;
+
+        string s = string.Format(format, value);
+        setValueText(s);
+    }
+
+    private void setValueText(string s)
+    {
+        if (null == ValueText)
+        {
+            Log.Error("not found value text");
+            return;
+        }
+
+        ValueText.text = s;
+    }
+}
