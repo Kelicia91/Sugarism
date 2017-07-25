a1cfb5c94886bafaf2f2d8962bd9feb591daba4e
Kelicia <kelicia91@gmail.com>
2017-07-25 22:36:31 +0900

    * add : feeling check panel script, prefab

---
+++ Sugarism/Assets/Scripts/Story/UI/FeelingCheckPanel.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class FeelingCheckPanel : Panel, IPointerClickHandler
+{
+    /********* Editor Interface *********/
+    // objects
+    public StoryCharacterPanel TargetPanel;
+
+    public SliderPanel FeelingPanel;
+    public SliderPanel ProgressPanel;
+    
+    //
+    private const string VALUE_FORMAT = "{0} %";
+
+
+    //
+    void Start()
+    {
+        FeelingPanel.SetNameText(Def.CMD_FEELING_CHECK);
+        ProgressPanel.SetNameText(Def.PROGRESS);
+        
+        Story.TargetCharacter tc = Manager.Instance.Object.StoryMode.TargetCharacter;
+
+        Target t = Manager.Instance.DTTarget[tc.Id];
+        TargetPanel.Set(tc.Id, false, Sugarism.EFace.Default, Sugarism.ECostume.Default, Sugarism.EPosition.Middle);
+
+        setFeeling(tc.Feeling);
+        setProgress(tc.LastOpenedScenarioNo);
+
+        tc.FeelingChangeEvent.Attach(onFeelingChanged);
+        tc.LastOpenedScenarioNoChangeEvent.Attach(onLastOpenedScenarioNoChanged);
+    }
+
+
+    private void setFeeling(int feeling)
+    {
+        float ratio = feeling / (float)Def.MAX_FEELING;
+        int percent = Mathf.FloorToInt(ratio * 100);
+
+        FeelingPanel.SetValue(percent, VALUE_FORMAT);
+    }
+
+    private void setProgress(int lastOpenedScenarioNo)
+    {
+        float ratio = lastOpenedScenarioNo / (float)Def.MAX_SCENARIO;
+        int percent = Mathf.FloorToInt(ratio * 100);
+
+        ProgressPanel.SetValue(percent, VALUE_FORMAT);
+    }
+
+
+    private void onFeelingChanged(int feeling)
+    {
+        setFeeling(feeling);
+    }
+
+    private void onLastOpenedScenarioNoChanged(int no)
+    {
+        setProgress(no);
+    }
+
+
+    // 오브젝트에서 포인터를 누르고 동일한 오브젝트에서 뗄 때 호출됩니다.
+    void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+    {
+        Log.Debug("click FeelingCheckPanel");
+
+        Hide();
+    }
+}
