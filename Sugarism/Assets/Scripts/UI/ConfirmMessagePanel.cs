5ebbe0ded05ff8e4839037593e09b8a34cfe9c9f
Kelicia <kelicia91@gmail.com>
2017-07-25 16:16:37 +0900

    * add : popup panel, confirm message panel - scripts, prefab

---
+++ Sugarism/Assets/Scripts/UI/ConfirmMessagePanel.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class ConfirmMessagePanel : Panel, IPointerClickHandler
+{
+    /********* Editor Interface *********/
+    // objects
+    public Text MessageText;
+
+    //
+    private UnityEngine.Events.UnityAction _clickHandler = null;
+
+
+    public override void Show()
+    {
+        Log.Error("not supported Show()");
+        return; // blocking base.Show()
+    }
+
+    public void Show(string msg, UnityEngine.Events.UnityAction clickHandler)
+    {
+        MessageText.text = msg;
+        _clickHandler = clickHandler;
+
+        base.Show();
+    }
+
+    // 오브젝트에서 포인터를 누르고 동일한 오브젝트에서 뗄 때 호출됩니다.
+    void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+    {
+        Log.Debug("click ConfirmMessagePanel");
+
+        Hide();
+
+        if (null != _clickHandler)
+            _clickHandler.Invoke();
+        else
+            Log.Debug("not found click handler");
+    }
+}
---
+++ Sugarism/Assets/Scripts/UI/PopupPanel.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+
+public class PopupPanel : Panel
+{
+    /********* Editor Interface *********/
+    // prefabs
+    public GameObject PrefConfirmMessagePanel;
+
+
+    //
+    private ConfirmMessagePanel _confirmMsg = null;
+    public ConfirmMessagePanel ConfirmMessage { get { return _confirmMsg; } }
+
+
+    //
+    void Awake()
+    {
+        create();
+
+        Manager.Instance.Object.EndNurtureEvent.Attach(onEndNurture);
+    }
+
+    //
+    void Start()
+    {
+        ConfirmMessage.Hide();
+    }
+
+    //
+    private void create()
+    {
+        GameObject o = null;
+
+        o = Instantiate(PrefConfirmMessagePanel);
+        o.transform.SetParent(transform, false);
+        _confirmMsg = o.GetComponent<ConfirmMessagePanel>();
+    }
+
+    //
+    private void onEndNurture()
+    {
+        ConfirmMessage.Show(Def.STORY_ENDING_START_MSG, onConfirmEndNurture);
+    }
+
+    private void onConfirmEndNurture()
+    {
+        Manager.Instance.Object.EndStory();
+    }
+
+}   // class
